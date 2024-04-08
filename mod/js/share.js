6c0577a67a53535d63ae235a018c709f4ab64a2e
3tfyi <2967394185@qq.com>
2024-04-09 00:14:55 +0800

    Site updated: 2024-04-09 00:14:54

--- mod/js/share.js
+++ mod/js/share.js
@@ -1,32 +1,32 @@
-var shareData = [
+﻿var shareData = [
     {
-        "getid": "336",
-        "url": "inVgG1udim1g",
-        "key": null
+        "getid":  336,
+        "url":  "inVgG1udim1g",
+        "key":  null
     },
     {
-        "getid": "523",
-        "url": "iXN0d1udiqdc",
-        "key": null
+        "getid":  343,
+        "url":  "iUX9h1udk5rc",
+        "key":  null
     },
     {
-        "getid": "1125",
-        "url": "i4xbU1udiqed",
-        "key": null
+        "getid":  523,
+        "url":  "iXN0d1udiqdc",
+        "key":  null
     },
     {
-        "getid": "343",
-        "url": "iUX9h1udk5rc",
-        "key": null
+        "getid":  577,
+        "url":  "iTnW81udkpvg",
+        "key":  null
     },
     {
-        "getid": "577",
-        "url": "iTnW81udkpvg",
-        "key": null
+        "getid":  974,
+        "url":  "iAlxK1udm8af",
+        "key":  null
     },
     {
-        "getid": "974",
-        "url": "iAlxK1udm8af",
-        "key": null
-}
+        "getid":  1225,
+        "url":  "i4xbU1udiqed",
+        "key":  null
+    }
 ]
