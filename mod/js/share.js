59e44331a477fa065f8a70160b1af600ff025931
3tfyi <2967394185@qq.com>
2024-04-09 00:35:11 +0800

    Site updated: 2024-04-09 00:35:11

--- mod/js/share.js
+++ mod/js/share.js
@@ -1,56 +1,56 @@
 ﻿var shareData = [
     {
-        "getid":  336,
+        "getid": "336",
         "url":  "inVgG1udim1g",
         "key":  null
     },
     {
-        "getid":  343,
+        "getid": "343",
         "url":  "iUX9h1udk5rc",
         "key":  null
     },
     {
-        "getid":  523,
+        "getid": "523",
         "url":  "iXN0d1udiqdc",
         "key":  null
     },
     {
-        "getid":  577,
+        "getid": "577",
         "url":  "iTnW81udkpvg",
         "key":  null
     },
     {
-        "getid":  974,
+        "getid": "974",
         "url":  "iAlxK1udm8af",
         "key":  null
     },
     {
-        "getid":  1225,
+        "getid": "1225",
         "url":  "i4xbU1udiqed",
         "key":  null
     },
     {
-        "getid":  1346,
+        "getid": "1346",
         "url":  "iPWxy1udx2id",
         "key":  null
     },
     {
-        "getid":  1358,
+        "getid": "1358",
         "url":  "irkRG1udx1wb",
         "key":  null
     },
     {
-        "getid":  1359,
+        "getid": "1359",
         "url":  "imZdJ1udx1yd",
         "key":  null
     },
     {
-        "getid":  1360,
+        "getid": "1360",
         "url":  "iKvjN1udx20f",
         "key":  null
     },
     {
-        "getid":  1361,
+        "getid": "1361",
         "url":  "i3Oh21udx21g",
         "key":  null
     }
